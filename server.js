0a897afe787648c74d2a8bd2edf8f2f733c360cd
João Vitor de Sousa <79536488+JoaoVitor-Dev@users.noreply.github.com>
2025-09-08 17:21:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "8234931064:AAGYg591ce7trOeYxVBl2s9G7E9uTZr72AQ"; 
+const TELEGRAM_CHAT_ID = "2093300021"; 
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
