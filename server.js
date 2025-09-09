afccecd94bc0da35d78a78ba1c78879786bd23fc
João Vitor de Sousa <79536488+JoaoVitor-Dev@users.noreply.github.com>
2025-09-09 08:31:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // server.js
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -8,8 +9,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "8234931064:AAGYg591ce7trOeYxVBl2s9G7E9uTZr72AQ"; 
-const TELEGRAM_CHAT_ID = "2093300021"; 
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
@@ -17,19 +18,24 @@ app.post("/send-location", async (req, res) => {
   const message = `A localização do usuário é:\nLatitude: ${latitude}\nLongitude: ${longitude}\nMaps: ${maps}`;
 
   try {
-    // Envia a localização para o Telegram
-    await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
-      chat_id: TELEGRAM_CHAT_ID,
-      text: message,
-    });
+    await axios.post(
+      `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
+      {
+        chat_id: TELEGRAM_CHAT_ID,
+        text: message,
+      }
+    );
 
     res.status(200).json({ success: true });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ success: false, message: "Erro ao enviar a localização para o Telegram." });
+    res
+      .status(500)
+      .json({ success: false, message: "Erro ao enviar a localização para o Telegram." });
   }
 });
 
-app.listen(8088, () => {
-  console.log("Servidor rodando na porta 8088");
+const PORT = process.env.PORT || 8088;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
